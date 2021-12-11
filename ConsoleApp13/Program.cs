101cdff9c2542a6abd3a9975bd2ae46069146e97
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-11 05:16:23 -0800

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string daxilEt;
+            string axtarilanSoz;
+            Console.WriteLine("Cumlenizi daxil edin");
+            daxilEt = Console.ReadLine();
+            string daxilETT = daxilEt.ToLower();
+            Console.WriteLine("Axtarmaq istediyiniz sozu yazin: ");
+            axtarilanSoz = Console.ReadLine();
+
+            int counter = 0;
+
+            string[] arr = daxilETT.Split(' ');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == axtarilanSoz)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
